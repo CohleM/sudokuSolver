0c82b87fba33a1398661d687c3850e1aa585bb15
Sanket Dhungel <68915135+Sanket009@users.noreply.github.com>
2020-10-01 22:54:16 +0545

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -5,7 +5,7 @@ cantHaveValues = defaultdict(list)
 trackList = dict()
 tList = list()
 # SampleBoards
-# board1
+# board A
 # board = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
 #          [5, 2, 0, 0, 0, 0, 0, 0, 0],
 #          [0, 8, 7, 0, 0, 0, 0, 3, 1],
@@ -16,7 +16,7 @@ tList = list()
 #          [0, 0, 0, 0, 0, 0, 0, 7, 4],
 #          [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
-#board2
+#board B 
 board = [[0, 0, 0, 2, 6, 0, 7, 0, 1],
          [6, 8, 0, 0, 7, 0, 0, 9, 0],
          [1, 9, 0, 0, 0, 4, 5, 0, 0],
